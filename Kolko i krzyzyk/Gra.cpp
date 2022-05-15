5e44d73620f66d34ff37a9234d4ddc208e9089ab
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-15 14:14:20 +0200

    GameLoop

---
+++ Kolko i krzyzyk/Gra.cpp
@@ -0,0 +1,50 @@
+#include "Gra.h"
+
+namespace Engine
+{
+	Gra::Gra(int width, int height, std::string tytul)
+	{
+		_dane->window.create(sf::VideoMode(width, height), tytul, sf::Style::Close | sf::Style::Titlebar);
+
+			this->Run( );
+	}
+
+	void Gra::Run()
+	{
+		float nowyCzas, czasKlatki, interpolacja;
+
+		float aktualnyCzas = this->_zegar.getElapsedTime().asSeconds();
+
+		float akumulator = 0.0f;
+
+		while (this->_dane->window.isOpen())
+		{
+			this->_dane->maszyna.PrzetworzZmianyState();
+
+			nowyCzas = this->_zegar.getElapsedTime().asSeconds();
+
+			czasKlatki = nowyCzas - aktualnyCzas;
+
+			if (czasKlatki > 0.25f)
+			{
+				czasKlatki = 0.25f;
+			}
+
+			aktualnyCzas = nowyCzas;
+			akumulator += czasKlatki;
+
+			while (akumulator >= dt)
+			{
+				this->_dane->maszyna.AktywnyState()->HandleInput();
+				this->_dane->maszyna.AktywnyState()->Update(dt);
+
+				akumulator -= dt;
+			}
+
+			interpolacja = akumulator / dt;
+			this->_dane->maszyna.AktywnyState()->Draw(interpolacja);
+		}
+	}
+}
+
+//zintegrowanie poprzednich manadzerow i state
