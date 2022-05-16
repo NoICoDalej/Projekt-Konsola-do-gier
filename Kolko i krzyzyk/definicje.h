08ea74317361899b7771575ccdd7d575b9439247
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-16 12:18:47 +0200

    definicje

---
+++ Kolko i krzyzyk/definicje.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define SCREEN_WIDTH 768
+#define SCREEN_HEIGHT 1136
+
+#define SPLASH_STATE_SHOW_TIME 3
+
+#define SPLASH_SCENA_TLO_SCIEZKAPLIK "splash screen/Resources/res/Splash Background.png"
