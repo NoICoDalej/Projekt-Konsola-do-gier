67d473df727f3bf045a4a60b7188aead5338e9d9
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-17 18:11:56 +0200

    definicje

---
+++ Kolko i krzyzyk/definicje.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 1136
+
+#define TITLE "Kolko"
+
+#define SPLASH_STATE_SHOW_TIME 3
+
+#define SPLASH_SCENA_TLO_SCIEZKAPLIK "Resources/res/Splash Background.png"
+#define MAIN_MENU_TLO_SCIEZKAPLIK "Resources/res/Main Menu Background.png"
+
+#define MAIN_MENU_TYTUL_SCIEZKAPLIK "Resources/res/Game Title.png"
+
+#define MAIN_MENU_PLAY_PRZYCISK "Resources/res/Play Button.png"
+
+#define MAIN_MENU_PLAY_PRZYCISK_ZEW "Resources/res/Play Button Outer.png"
