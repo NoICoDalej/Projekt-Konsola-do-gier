69d463dc442b7c5776bee780835ba97221d70273
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-19 14:23:09 +0200

    Update StateGry.h

--- Kolko i krzyzyk/StateGry.h
+++ Kolko i krzyzyk/StateGry.h
@@ -4,6 +4,8 @@
 #include "State.h"
 #include "Gra.h"
 
+#include "AI.h"
+
 namespace Engine
 {
 	class StateGry : public State
@@ -18,13 +20,29 @@ namespace Engine
 		void Draw(float dt);
 
 	private:
+		void WczytyajElementyGrid();
+
+		void SprawdzIPolozElement();
+
+		void SprawdzCzyGraczWygral(int turn);
+		void SprawdzCzy3ElementyPasuja(int x1, int y1, int x2, int y2, int x3, int y3, int elementDoSprawdzenia);
+
 		DaneGryRef _dane;
 
 		sf::Sprite _tlo;
 
 		sf::Sprite _pauzaPrzycisk;
 
+		sf::Sprite _gridSprite;
+
+		sf::Sprite _gridElementy[3][3];
+		int gridTablica[3][3];
+
 		int turn;
 		int graState;
+
+		AI* ai;
+
+		sf::Clock _zegar;
 	};
 }
