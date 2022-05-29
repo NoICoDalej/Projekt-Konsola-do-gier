57e59364f8c95533c09e1191812baa9d58699a24
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-29 22:31:22 +0200

    Main

---
+++ MiniConsoleGames/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Gra.h"
+#include "definicje.h"
+
+int main()
+{
+	Engine::Gra(SCREEN_WIDTH, SCREEN_HEIGHT, "Mini Console Games");
+
+	return EXIT_SUCCESS;
+};
+//main
