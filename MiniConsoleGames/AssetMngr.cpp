2ccb3331a4d32eb943cc6c95baf4405eac463697
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-29 22:30:05 +0200

    Menedżer Asetów

---
+++ MiniConsoleGames/AssetMngr.cpp
@@ -0,0 +1,36 @@
+#include "AssetMngr.h"
+
+namespace Engine
+{
+	void AssetMngr::WczytajTexture(std::string nazwa, std::string nazwaPliku)
+	{
+		sf::Texture tex;
+
+		if (tex.loadFromFile(nazwaPliku))
+		{
+			this->_textures[nazwa] = tex;
+		}
+	}
+
+	sf::Texture &AssetMngr::WczytajTexture(std::string nazwa)
+	{
+		return this->_textures.at(nazwa);
+	}
+
+	void AssetMngr::WczytajFont(std::string nazwa, std::string nazwaPliku)
+	{
+		sf::Font font;
+
+		if (font.loadFromFile(nazwaPliku))
+		{
+			this->_fonts[nazwa] = font;
+		}
+	}
+
+	sf::Font &AssetMngr::WczytajFont(std::string nazwa)
+	{
+		return this->_fonts.at(nazwa);
+	}
+}
+
+//mened¿er assetów
---
+++ MiniConsoleGames/AssetMngr.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <map>
+
+#include <SFML/Graphics.hpp>
+
+namespace Engine
+{
+	class AssetMngr
+	{
+	public:
+		AssetMngr() {}
+		~AssetMngr() {}
+
+		void WczytajTexture(std::string nazwa, std::string nazwaPliku);
+		sf::Texture &WczytajTexture(std::string nazwa);
+
+		void WczytajFont(std::string nazwa, std::string nazwaPliku);
+		sf::Font &WczytajFont(std::string nazwa);
+
+	private:
+		std::map<std::string, sf::Texture> _textures;
+		std::map<std::string, sf::Font> _fonts;
+	};
+
+
+}
+
+//mened¿er assetów
