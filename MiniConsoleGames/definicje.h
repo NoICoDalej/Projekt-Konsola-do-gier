fe6a5ca0c8b2c3057b65efc4fcdf2e1b440bb452
NoICoDalej <kacper.szczygielski@uczen.zsk.poznan.pl>
2022-05-29 22:30:10 +0200

    definicje

---
+++ MiniConsoleGames/definicje.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 1136
+
+#define SPLASH_STATE_SHOW_TIME 3
+
+//kolko i krzyzyk
+
+#define SPLASH_SCENA_TLO_SCIEZKAPLIK "Resources/res/Splash Background.png"
+#define MAIN_MENU_TLO_SCIEZKAPLIK "Resources/res/Main Menu Background.png"
+#define GAME_MENU_TLO_SCIEZKAPLIK "Resources/res/Main Menu Background.png"
+#define PAUZA_TLO_SCIEZKAPLIK "Resources/res/Main Menu Background.png"
+
+#define MAIN_MENU_TYTUL_SCIEZKAPLIK "Resources/res/Game Title.png"
+#define KOLKO_I_KRZYZYK_PLAY "Resources/res/kolkoikrzyzyk.png"
+#define WZNOW_PRZYCISK "Resources/res/Resume Button.png"
+#define HOME_PRZYCISK "Resources/res/Home Button.png"
+#define POWTORZ_PRZYCISK "Resources/res/Retry Button.png"
+
+#define PAUZA_PRZYCISK "Resources/res/Pause Button.png"
+#define X_ELEMENT_SCIEZKAPLIK "Resources/res/X.png"
+#define O_ELEMENT_SCIEZKAPLIK "Resources/res/O.png"
+#define X_WYGRANY_ELEMENT_SCIEZKAPLIK "Resources/res/X Win.png"
+#define O_WYGRANY_ELEMENT_SCIEZKAPLIK "Resources/res/O Win.png"
+
+#define GRID_SPRITE_SCIEZKAPLIK "Resources/res/Grid.png"
+
+#define X_ELEMENT 8
+#define O_ELEMENT 0
+#define PUSTY_ELEMENT -1
+#define GRACZ_ELEMENT X_ELEMENT
+#define AI_ELEMENT O_ELEMENT
+
+#define STATE_W_TRAKCIE_GRY 98
+#define STATE_ZAPAUZOWANE 97
+#define STATE_WYGRAL 96
+#define STATE_PRZEGRAL 95
+#define STATE_KLADZIE_ELEMENT 94
+#define STATE_AI_W_TRAKCIE_GRY 93
+#define STATE_REMIS 92
+
+#define CZAS_PRZED_POKAZANIEM_KONIEC_GRY 3
+
+// snake
+
+#define SNAKE_PLAY "Resources/res/snake.png"
+#define CZERWONY "Resources/res/red.png"
+#define ZIELONY "Resources/res/green.png"
+#define BIALY "Resources/res/white.png"
+
+//mahjong
+
+#define MAHJONG_PLAY "Resources/res/mahjong.png"
+#define MAHJONG_TLO_SCIEZKAPLIK "Resources/res/background_mahjong.png"
+#define MAHJON_KLOCKI_SCIEZKAPLIK "Resources/res/tiles_mahjong.png"
+#define MAHJON_MAPA_SCIEZKAPLIK "Resources/res/map.txt"
+
